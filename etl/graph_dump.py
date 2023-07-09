29a7c7d4bb8537c1d28998a206e2b13ac979aea5
zichen719 <VictoriaShi2025@u.northwestern.edu>
2023-07-09 03:14:20 -0400

    Update graph_dump.py

--- etl/graph_dump.py
+++ etl/graph_dump.py
@@ -1,11 +1,11 @@
 from data_model.nodes import NODES
 from data_model.relationships import RELATIONSHIPS
 from data_model.constraints import CONSTRAINTS
-from etl.utils import ingest_nodes, ingest_relationships, execute_cypher_query
+from etl.utils import ingest_nodes, ingest_relationships, run_cypher_query
 
 
 for constraint in CONSTRAINTS:
-    execute_cypher_query(constraint)
+    run_cypher_query(constraint)
 
 for node in NODES:
     ingest_nodes(node)
